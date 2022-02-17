e31b345ecbb8ff95b086486fba81718b7b23896a
Mehdi-meg <meghiramehdi.etudiant@gmail.com>
2022-02-17 19:21:15 +0100

    modal appel

--- frontend/src/pages/messagerie/components/Modal.js
+++ frontend/src/pages/messagerie/components/Modal.js
@@ -22,6 +22,33 @@ function MyModal({ modal, setModal }) {
         setIsModalVisible(modal.isVisible)
     }, [modal.isVisible])
 
+    // const ClickToDelete = () => {
+    //     setIsModalDeleteVisible(true)
+    // }
+
+    // const handleDeleteOk = () => {
+    //     setIsModalDeleteVisible(false)
+    // }
+
+    // const handleDeleteCancel = () => {
+    //     setIsModalDeleteVisible(false)
+    // }
+
+    // const SupprimerPost = (e) => {
+    //     e.preventDefault()
+    //     setIsDeleteLoading(true)
+    //     setTimeout(() => {
+    //         SUPPRIMER_POST()
+    //         setIsDeleteLoading(false)
+    //         setIsModalDeleteVisible(false)
+    //     }, 2000)
+    // }
+
+    // const annuler = (e) => {
+    //     e.preventDefault()
+    //     setIsModalDeleteVisible(false)
+    // }
+
     const showModal = () => {
         setIsModalVisible(true)
     }
@@ -59,6 +86,64 @@ function MyModal({ modal, setModal }) {
     return (
         <>
             <Modal
+                visible={isModalVisible}
+                // onOk={() => {
+                //     var modalInfo = modal
+                //     setModal({
+                //         isVisible: false,
+                //         id_sender: 0,
+                //         id_reciever: 0,
+                //         video: true,
+                //     })
+                //     navigate(
+                //         `/appel-video/${modalInfo.id_reciever}/${modalInfo.id_sender}/${modalInfo.video}`
+                //     )
+                // }}
+                onCancel={handleClose}
+                style={{ top: 100 }}
+                width={400}
+                footer=""
+                className="p-1 ring bg-red-100 rounded-xl"
+            >
+                <div className="text-center space-y-2">
+                    <span className="font-bold text-lg">
+                        {reciever.lastname} {reciever.firstname}
+                    </span>
+                    <p className="text-md">
+                        {modal.video
+                            ? 'vous envoi un appel video'
+                            : 'vous envoi un appel audio'}
+                    </p>
+
+                    <div className="flex flex-col w-2/3 m-auto justify-center space-y-2">
+                        <button
+                            className="bg-black text-white p-2  text-sm rounded-2xl hover:opacity-80 "
+                            onClick={() => {
+                                var modalInfo = modal
+                                setModal({
+                                    isVisible: false,
+                                    id_sender: 0,
+                                    id_reciever: 0,
+                                    video: true,
+                                })
+                                navigate(
+                                    `/appel-video/${modalInfo.id_reciever}/${modalInfo.id_sender}/${modalInfo.video}`
+                                )
+                            }}
+                        >
+                            Accepter
+                        </button>
+                        <button
+                            className="bg-white text-black border border-solid p-1 text-lg rounded-2xl hover:bg-sky-100  "
+                            onClick={handleClose}
+                        >
+                            Annuler
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+
+            {/* <Modal
                 title="Appel DiveIn"
                 visible={isModalVisible}
                 onOk={() => {
@@ -78,7 +163,7 @@ function MyModal({ modal, setModal }) {
                     {modal.video ? ' Video...' : ' Audio...'}
                     (clickez sur ok pour accepter ou sur cancel pour refuser)
                 </p>
-            </Modal>
+            </Modal> */}
         </>
     )
 }
